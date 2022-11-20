8cfcb34f4f8bd86ca36afceeba72d989760cd153
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-20 16:00:07 -0300

    added the new routes to the routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import AuthMiddleware from "../src/middlewares/auth";
 import UsersController from "./users/controller";
+import login from "./services/login";
 
 const routes = express.Router();
 
 //Users
 routes.post("/users", UsersController.create);
+routes.get("/balance", AuthMiddleware, UsersController.balance);
+
+//Services
+routes.post("/login", login);
 
 export default routes;

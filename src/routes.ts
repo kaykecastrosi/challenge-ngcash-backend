1f144da979b6963a81c4219a098867bd359d2abb
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-20 17:28:46 -0300

    added transactions history route to routes file

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import AuthMiddleware from "../src/middlewares/auth";
 import UsersController from "./users/controller";
 import login from "./services/login";
 import transaction from "./services/transaction";
+import transactionsHistory from "./services/transactionsHistory";
 
 const routes = express.Router();
 
@@ -13,5 +14,6 @@ routes.get("/balance", AuthMiddleware, UsersController.balance);
 //Services
 routes.post("/login", login);
 routes.put("/transaction", AuthMiddleware, transaction);
+routes.get("/realizedTransactions", AuthMiddleware, transactionsHistory);
 
 export default routes;

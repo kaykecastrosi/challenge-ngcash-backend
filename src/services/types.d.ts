bc0f9f68303f712b88770b073789a36d28b2a538
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-20 16:34:43 -0300

    created a type for a transaction requisition

--- src/services/types.d.ts
+++ src/services/types.d.ts
@@ -1,4 +1,11 @@
+import { Decimal } from "@prisma/client/runtime";
+
 export type LoginRequisition = {
   username: string;
   password: string;
+};
+
+export type TransactionRequisition = {
+  amount: Decimal;
+  receiverUsername: string;
 };

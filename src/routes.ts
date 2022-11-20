844d2109a3a0a6ca9e8475d22d0708f7e7c80d7c
Kayke Castro <kaykecastrodev@gmail.com>
2022-11-20 16:35:02 -0300

    added transaction route on the routes file

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import AuthMiddleware from "../src/middlewares/auth";
 import UsersController from "./users/controller";
 import login from "./services/login";
+import transaction from "./services/transaction";
 
 const routes = express.Router();
 
@@ -11,5 +12,6 @@ routes.get("/balance", AuthMiddleware, UsersController.balance);
 
 //Services
 routes.post("/login", login);
+routes.put("/transaction", AuthMiddleware, transaction);
 
 export default routes;
